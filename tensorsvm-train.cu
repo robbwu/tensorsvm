bab0d0824ae20841d2b6dd1fcf5c8b04c5d53c0d
Panruo Wu <pwu7@uh.edu>
2019-10-01 18:17:34 -0500

    [perf:writemodel] disabling the old intercept b computation; use the new one instead.

--- tensorsvm-train.cu
+++ tensorsvm-train.cu
@@ -264,6 +264,7 @@ double writemodel(char *path, double *X,  double C, double *U)
 			b = 0;
 		}
 	} else if (T==2) { // RBF Kernel
+        /*
         { // computing b based on original model; could have large variance if LRA is inaccurate
             double acc = 0;
             std::vector<double> bs(std::min(nBSV,500), 0);
@@ -290,10 +291,11 @@ double writemodel(char *path, double *X,  double C, double *U)
                 sumsq += (bs[j]-b)*(bs[j]-b);
             printf("original mean b=%.6e std b=%.6e, #samples=%d\n ", b, sqrt(sumsq/bs.size()), bs.size());
         }
+        */
         {
             double acc = 0;
-            std::vector<double> bs(std::min(nBSV,100), 0);
-            for (int j=0; j<std::min(nBSV,100); j++) {
+            std::vector<double> bs(std::min(nBSV,50), 0);
+            for (int j=0; j<std::min(nBSV,50); j++) {
                 int jj = iBSV[j];
                 double yj = LABELS[jj];
                 for (int i=0; i<nSV; i++) {
@@ -308,7 +310,7 @@ double writemodel(char *path, double *X,  double C, double *U)
                 bs[j] = yj;
                 // printf("y[%d]=%.3e\n", jj, yj);
             }
-            b = acc/std::min(nBSV,100);
+            b = acc/std::min(nBSV,50);
             double sumsq = 0;
             for( int j=0; j<bs.size(); j++ ) 
                 sumsq += (bs[j]-b)*(bs[j]-b);
